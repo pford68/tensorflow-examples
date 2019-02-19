58ae0e8735a080b22cf459fd919b5a946c38d4f9
Philip Ford <paford@us.ibm.com>
2019-02-19 10:51:38 -0500

    Added comments

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 
+'''
+Final version of code from branch simplest-version.
+See that branch for notes.
+'''
 x = tf.constant(1.0, name='input')
 w = tf.Variable(0.8, name='weight')
 y = tf.multiply(w, x, name='output')
